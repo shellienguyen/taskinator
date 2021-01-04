64e3a9b3a4fb0126d7abd2d86e1a32ab9f98264c
sidonie <shelliednnguyen@gmail.com>
2021-01-04 14:52:04 -0800

    code refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
 var formEl = document.querySelector( "#task-form" );
 var taskToDoEl = document.querySelector( "#tasks-to-do" );
 
-var createTaskHandler = function( event ) {
+var taskFormHandler = function( event ) {
     
    event.preventDefault();
-
    var taskNameInput = document.querySelector( "Input[ name = 'task-name' ]" ).value;
    var taskTypeInput = document.querySelector( "select[ name = 'task-type' ]").value;
-   console.log( "taskNameInput: " + taskNameInput );
-   console.log( "taskTypeInput: " + taskTypeInput );
+
+   // Package up data as an object
+   var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput
+   };
+
+   //Send it as an argument to createTaskEl
+   createTaskEl( taskDataObj );
+};
+
+var createTaskEl = function( taskDataObj ) {
 
    // Create list item
    var listItemEl = document.createElement( "li" );
@@ -21,11 +30,11 @@ var createTaskHandler = function( event ) {
    taskInfoEl.className = "task-info";
 
    // Add HTML content to div
-   taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
+   taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskDataObj.name + "</h3><span class = 'task-type'>" + taskDataObj.type + "</span>";
    listItemEl.appendChild( taskInfoEl );
 
    // Add entire list item to list
    taskToDoEl.appendChild( listItemEl );
-};
+}
 
-formEl.addEventListener( "submit", createTaskHandler );
+formEl.addEventListener( "submit", taskFormHandler );

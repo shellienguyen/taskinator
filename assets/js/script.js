8de3883c42a5325a082a5e5dff9abe5c6d8916a5
sidonie <shelliednnguyen@gmail.com>
2021-01-04 12:32:29 -0800

    capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,26 @@ var createTaskHandler = function( event ) {
     
    event.preventDefault();
 
+   var taskNameInput = document.querySelector( "Input[ name = 'task-name' ]" ).value;
+   var taskTypeInput = document.querySelector( "select[ name = 'task-type' ]").value;
+   console.log( "taskNameInput: " + taskNameInput );
+   console.log( "taskTypeInput: " + taskTypeInput );
+
+   // Create list item
    var listItemEl = document.createElement( "li" );
    listItemEl.className = "task-item";
-   listItemEl.textContent = "This is a new task.";
+
+   // Create div to hold task info and add to list item
+   var taskInfoEl = document.createElement( "div" );
+
+   // Give it a class name
+   taskInfoEl.className = "task-info";
+
+   // Add HTML content to div
+   taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
+   listItemEl.appendChild( taskInfoEl );
+
+   // Add entire list item to list
    taskToDoEl.appendChild( listItemEl );
 };
 
